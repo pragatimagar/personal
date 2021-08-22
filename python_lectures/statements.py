ac3ba66ace5a7b68ea2b6ef51d940164b0f856ac
pragatimagar <pragatimagar28sep@gmail.com>
2021-08-22 14:12:59 +0530

    added python lectures

---
+++ python_lectures/statements.py
@@ -0,0 +1,59 @@
+# print('hello world')
+# print('hello world')
+#--------------------------------------------------------------------------------
+# a=15
+# b=20
+# if (a>b):
+#     print('a is greater')
+# elif(a<b):
+#     print('a is less')
+# else:
+#     print('a is euals to b')    
+#-------
+# a=int(input('enter value of a'))
+# b=int(input('enter value of b'))
+# if(a>b):
+#     print('a is greater')
+#     if ((a-b)>=3):
+#         print('difference is {}'.format(a-b))
+      
+# elif(a<b):
+#     print('a is less')
+# else:    
+#     print('both equal')   
+#--------------------------------
+# name = input('enter name').lower()
+# if name == 'pragati' or name == 'aishu':
+#     print('hello')
+# elif name == 'balaji':
+#     print('hiiiii')
+# else:
+#     print('hmmmm')       
+
+#------For loop---------
+# list2 = [8,10,'name',15]
+# for a in list2:
+#     print(a)     
+# list1 = (15,10,7,8).sort()
+# for v in list1:
+#     print(v)
+# list3 = {'k1':4,'k2':'name','k3':15}
+# for a,b in list3:
+#     print(a)
+#     print(b)
+# list3 = {'k1':4,'k2':'name','k3':15}
+# for a,b in list3.items():
+#     print(a)
+#     print('value is',b)
+#------------------------
+# list1 =[1,2,3,4,5,6,7,8]
+# for i in list1:
+    
+#     if not (i%2):
+#         print(i)
+#--------------------------------
+# lst ='My Name is'
+# for i in lst.upper():
+#     print(i)
+
+#--------git------------
