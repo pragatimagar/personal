1fd53adfc678fbf5333903d3d3e76e34b36db9d6
pragatimagar <pragatimagar28sep@gmail.com>
2021-08-24 16:46:17 +0530

    24/08/21

---
+++ python_lectures/practice3.py
@@ -0,0 +1,20 @@
+# if (1==1):
+#     print('i am true')
+# else:
+#     print('i am false')    
+# print("executed")
+# a = []
+# if a :
+#     print('i am in if statement')
+#     if (1<2):
+#         print('i am correct')
+# else:
+#     print('i am in else statement')    
+# if 1:
+#     if (1==2):
+#         print (' I am true')
+#     print('hello')
+#     if (1==1):
+#         print('i am in if statement')
+#     else:
+#         print('i am not true')        
