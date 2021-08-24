5ab40bdc49fc3adbc44ad207448d6af1c4908471
pragatimagar <pragatimagar28sep@gmail.com>
2021-08-24 18:15:41 +0530

    modified

---
+++ python_lectures/prac.py
@@ -0,0 +1,22 @@
+# list3 = {'k1':4,'k2':'name','k3':15}
+# for a,b in list3.items():
+#    print(a)
+#    print('value is',b)
+# list2 = {'k1':25,'k2':30,'k3':7}
+# for v,x in list2.items():
+#     print(v)
+#     print(x)
+# list1 = [1,2,3,4,5,6,7,8]
+# for i in list1:
+#     if not(i%2):
+#         print(i)
+
+# a = 'This is string'
+# for v in a.upper():
+#     print(v)
+# list = [1,2,5,3,6,7,8,9,10]
+# for z in list:
+#     if not((z%2)==0):
+#         print(z)
+
+print('z')
--- python_lectures/practice3.py
+++ python_lectures/practice3.py
@@ -18,3 +18,50 @@
 #         print('i am in if statement')
 #     else:
 #         print('i am not true')        
+# x = False
+# if x:
+#     print('It is true')
+# else:
+#     print('It is not True')   
+# Fruit = 'Mango'
+# if Fruit == 'Apple':
+#     print('Take 3')
+# elif Fruit == 'Mango':
+#     print('seperate out from others')
+# else:
+#     print('no same fruit') 
+# v =int(input('enter value of v'))
+# x =int(input('enter value of x')) 
+# if (v>x):
+#     print('it is if statement') 
+#     if ((v-x)<=2):   
+#         print('difference is {}'.format(v-x))
+# else:
+#     print('it is else statement')       
+# a =int(input('enter a'))
+# b =int(input('enter b'))
+# if (a>b):
+#     print('go to b')
+#     if ((a*b)==20):
+#         print('multiplication value is {}'.format(a*b))
+# elif (a<b):
+#     print('go to a') 
+# else:
+#     print('print none') 
+#    
+#-----------For loop-------------------------------
+# lst5 = [5,14,'name','set',160]
+# for v in lst5:
+#     print(v)
+# list = [15,12,25,2]
+# for a in list.sort():
+#     print(a) 
+# list3 = {'k1':10,'k2':'name','k3':25}
+# for a,b in list3:
+#     print(a)
+#     list3 = {'k1':4,'k2':'name','k3':15}
+list3 = {'name':'pragati','fruit':'apple'}
+for a,b in list3:
+      print(a)
+      print(b)
+    
