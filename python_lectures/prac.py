07bf1a3d363d25d33405f36e7734f50f0710efb8
pragatimagar <pragatimagar28sep@gmail.com>
2021-08-26 15:47:21 +0530

    26/08/21

--- python_lectures/prac.py
+++ python_lectures/prac.py
@@ -18,5 +18,65 @@
 # for z in list:
 #     if not((z%2)==0):
 #         print(z)
+''' new folder '''
+# a = 10
+# print(type(a))
 
-print('z')
+#---------------------Comparison operator----------
+
+# a = 3
+# b = 4
+# print('The value of 3+4 is',3+4)
+
+# a = 25
+# a += 2
+# print(a)
+
+
+#---------------Boolean operaator----------------
+
+# bool1 = True
+# bool2 = False
+# print('The value of  is',(not bool1))
+
+# a = '2846'
+# a = int(a)
+# print(type(a))
+# print (a+5)
+
+# a = float(25)
+# print(a)
+
+#---------iNPUT FUNCTION-----------------
+
+# a = int(input('Enter your age'))
+# print(type(a))
+
+#-----add two no--------
+# a = 53
+# b = 13
+# print('The sum of a and b is :',(a+b))
+
+#-------------------------------------
+# a = 14
+# b = 5
+# print('The reminder when a is devided by b is ',(a%b))
+
+
+#-----------Comparision operator------------
+# a = 25
+# b = 62
+
+# print( not a<b)
+
+#---------Avg of two nos------------
+
+# a = int(input('Enter first number: '))
+# b = int(input('Enter second number: '))
+# avg = (a+b)/2 # o/p is float values
+# avg = int((a+b)/2) # o/p is int value
+# print('The average of a and b is ',avg)
+
+a = int(input('Enter a value'))
+square = (a**2)
+print('the square of a is ',square)
