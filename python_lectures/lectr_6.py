dc3aa1f90309e88345ac0d5e5374da98e6f29e0b
pragatimagar <pragatimagar28sep@gmail.com>
2021-08-28 13:07:58 +0530

    28/08

---
+++ python_lectures/lectr_6.py
@@ -0,0 +1,65 @@
+#------------while loop----------------
+
+# x = 0
+# while x<10 :
+    # print('The value of ',x)
+    # if (x==4):
+        # print('value is equals to ',x)
+        # break
+    # else:
+        # x = x+2
+        # print('continue list...')
+        # continue
+    
+#-------------Range------------------
+
+# list1 = list(range(0,51))
+# list1 = list(range(0,51,3))
+# print1(list1)
+
+
+#-----------------enumerate--------------------------
+
+# print(list(enumerate('pragati')))
+# a = dict(enumerate('pragati'))
+# print(a.keys())
+
+# for i,j in enumerate('pragati'):
+#    print('The index value is {} and variable is {}'.format(i,j))
+
+
+#-------------zip-----------------
+
+# list1 = [2,5,6,8]
+# # list2 = ['abcd']
+# list2 = ['a','b','c','d']
+
+
+# print(list(zip(list1,list2)))
+
+
+#-------------in-----------------
+
+# list1 = ['name','age','fruit']
+# if 'age' in list1:
+#     print('name found')
+
+#-----------------min,max----------------
+#find min and max value from list
+
+# list1 = [25,41,69,72,89]
+# # print(min(list1))
+# print(max(list1))
+
+#--------List comprehension----------------
+
+# new = [10,15,16,19,21]
+
+# list1 = [a+2 for a in new]
+# print(list1)
+
+
+
+# lst = [z for z in range(11) if z%2==0]
+# print(lst)
+
