3d068a07757ac8c22a4884b02a05d675aff968ce
pragatimagar <pragatimagar28sep@gmail.com>
2021-09-15 23:27:48 +0530

    updating

---
+++ pragati/line.py
@@ -0,0 +1,9 @@
+# content = False
+# i = 1
+# with open('another.txt') as f:
+#     while content:
+#         content = f.readline()
+#         if 'yes' in content.lower():
+            
+#             print(f"yes is present in line number{i}")
+#         i+=1    
---
+++ pragati/rename_file.py
@@ -0,0 +1,14 @@
+#rename file 
+
+
+# import os
+# oldfile = 'sample2.txt'
+# new_file = 'rename_by_python.txt'
+
+# with open(oldfile) as f:
+#     content = f.read()
+
+# with open(new_file,'w') as f:
+#     f.write(content)   
+
+# os.remove(oldfile) #delete previous file
