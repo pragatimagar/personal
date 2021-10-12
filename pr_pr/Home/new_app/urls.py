c2513dde846d64b34691161e18dfbe0b3bf4c007
pragatimagar <pragatimagar28sep@gmail.com>
2021-10-12 12:29:19 +0530

    updated

--- pr_pr/Home/new_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.index,name='new_app'),
-    path('about',views.about,name='about'),
-    path('services',views.services,name='services'),
-    path('contact',views.contact,name='contact')
-]
